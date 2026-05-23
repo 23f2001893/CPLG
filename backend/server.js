51471b74693fe1455abc580e1d0c7d562b486116
Lalit Pradhan <23f2001893@ds.study.iitm.ac.in>
2026-05-23 23:53:33 +0530

    server setup

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const cors = require("cors");
+const express = require("express");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Backend running successfully");
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000")
+});
+
