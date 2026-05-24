61f9132738775b60f11f19e5375855a7fc368d69
amitpradhan <pradhanamit762@gmail.com>
2026-05-24 22:11:17 +0530

    building registration api

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,15 @@ app.use(express.json())
 app.get("/", (req, res) => {
     res.send("Backend running successfully");
 });
+app.post("/api/register", (req,res) =>{
+    console.log(req.body);
+    res.send("registration API working")
+
+} );
+
+
+
+
 
 app.listen(5000, () => {
     console.log("Server running on port 5000")
