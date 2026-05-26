83128b8bc19e09957200f6bf676e8e3837772289
Lalit Pradhan <23f2001893@ds.study.iitm.ac.in>
2026-05-27 00:29:11 +0530

    some

---
+++ frontend/src/Register.jsx
@@ -0,0 +1,29 @@
+function Register(){
+    return(<>
+        <h1>REGISTRATION FORM</h1>
+        <form >
+            <label>username:  </label>
+            <input type="text" /> <br/>
+            <label >password:  </label>
+            <input type="password" />
+            <label>CIRCLE:  </label>
+            <input type="text" /> <br/>
+            <label>DIVISION:  </label>
+            <input type="text" /> <br/>
+            <label>SUBDIVSION:  </label>
+            <input type="text" /> <br/>
+            <label>SUB OFFICE:  </label>
+            <input type="text" /> <br/>
+            <label>BRANCH OFFICE:  </label>
+            <input type="text" /> <br/>
+            <label>EMP_ID:  </label>
+            <input type="text" /> <br/>
+            <label>Mobile no.</label>
+            <input type="int" /> <br/>
+
+        </form>
+        </>
+    )
+}
+
+export default Register;
